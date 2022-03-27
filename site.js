ea53fbca51dad5e211d2b51cd0f53d5f7909fb94
moraruv0 <75850366+moraruv0@users.noreply.github.com>
2022-03-27 19:55:25 +0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,29 @@
+document.querySelector("#switch")
+    .addEventListener("click", changeTheme)
+
+var theme = localStorage.getItem("theme")
+setTheme(theme)
+
+function setTheme(theme) {
+    let css = document.querySelector("#darkcss")
+    css.disabled = theme == "light" ? true : false
+
+    let symbol = css.disabled ? "&#9789;" : "&#9788;"
+    document.querySelector("#switch").innerHTML = symbol
+
+    localStorage.setItem("theme", theme)
+}
+
+function changeTheme() {
+    theme =  theme == "light" ? "dark" : "light"
+    setTheme(theme)
+
+    // let css = document.querySelector("#darkcss")
+    // css.disabled = !css.disabled
+
+    // let symbol = css.disabled ? "&#9789;" : "&#9788;"
+    // document.querySelector("#switch").innerHTML = symbol
+
+    // let theme = css.disabled ? "light" : "dark"
+    // localStorage.setItem("theme", theme)
+}
